71d47d7b52554f619581f0aaf348797a292efca0
Saebom Kim <rlatoqha0101@naver.com>
2022-09-21 11:02:45 +0900

    nodejs

--- 02.http/09.request.js
+++ 02.http/09.request.js
@@ -3,7 +3,8 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
     let pathname = url.parse(req.url).pathname;
-    console.log(pathname);
+    let method = req.method;
+    console.log(pathname, method);
 
     res.writeHead(200, {'Content-Type': 'text/html'}); // 이 코드가 없어도 돌아감 (생략가능)
     res.end(`<h1>${pathname}</h1>`);
